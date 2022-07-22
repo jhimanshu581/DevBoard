cb6dd125f7fc1dd015c9337e6a40bd859f0163e5
deepalib <deepalib@vmware.com>
2022-07-22 23:30:33 +0530

    roles service

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -10,8 +10,21 @@ export class RolesComponent implements OnInit {
   constructor() { }
 
   selected: boolean;
+  serviceId: string;
+  name: string;
+  displayName: string;
+  description: string;
+  permissions: string;
+  onAccess: boolean;
+  visible: boolean;
+  type:string;
+  composable:boolean; 
+  bundled:boolean;
+  
 
   ngOnInit(): void {
   }
 
+  createRole(): void {
+  }
 }
---
+++ src/app/services/roles.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { urls } from './urls';
+import { CookiesService } from './cookies.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class RolesService {
+
+    constructor(private http: HttpClient, private _cookie :CookiesService) { }
+  
+    roleService(refreshToken: string){
+  
+        return this.http.post(urls.gitLabCreateGroupURL, {
+            name : refreshToken,
+            path : refreshToken
+          });
+    }
+  
+    createRoleService(serviceId: string, name: string, displayName: string, description:string, permissions:string, onAccess: string, visible: string, type:string, composable:string, bundled:string ){
+      let formdata = new FormData();
+      formdata.append('SERVICE ID',serviceId);
+      formdata.append('NAME', name);
+      formdata.append('DISPLAY NAME', displayName);
+      formdata.append('DESCRIPTION', description);
+      formdata.append('PERMISSIONS', permissions);
+      formdata.append('On Access', onAccess);
+      formdata.append('Visible', visible);
+      formdata.append('TYPE', type);
+      formdata.append('Composable',composable);
+      formdata.append('Bundled', bundled);
+      return this.http.post(urls.createRole, formdata);
+    }
+  }
+
+
